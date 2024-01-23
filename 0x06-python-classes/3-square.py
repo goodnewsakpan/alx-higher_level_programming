e7ef0ec17ba95cc6c447f6e0997e0294709c28ee
goodnewsakpan <goodnewsakpan123@gmail.com>
2024-01-23 19:42:22 +0100

    function to handle square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A square Class"""
+
+
+class Square:
+    """A class"""
+
+    def __init__(self, size=0):
+        """Square definition"""
+        try:
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        except (TypeError, ValueError) as error:
+            print(error)
+
+    def area(self):
+        """Area of size square"""
+        return self.__size ** 2
