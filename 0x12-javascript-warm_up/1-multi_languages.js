b142926854cf4c64258c47645a2ce991b8e51671
goodnews akpan <goodnewsakpan123@gmail.com>
2024-04-08 19:53:47 +0100

    script to print three lines at once

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myVar = 'C is fun';
+const secondVar = 'Python is cool';
+const thirdVar = 'JavaScript is amazing';
+
+console.log(myVar);
+console.log(secondVar);
+console.log(thirdVar);
