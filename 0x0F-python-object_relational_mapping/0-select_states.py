002fa980d611ac37827afe9df52ce72b8d3b5baf
goodnews akpan <goodnewsakpan123@gmail.com>
2024-04-15 19:27:25 +0100

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,37 @@
-#!usr/bin/python3
-""" script that lists all
-states from the database hbtn_0e_0_usa
+#!/usr/bin/python3
 """
+MySQL script that lists all states from the database hbtn_0d_usa.
+"""
+from sys import argv
+
+from MySQLdb import connect
 
 
-import MySQLdb
-import sys
+def main(username, password, database):
+    """
+    Main function.
+    :param username:
+    :param password:
+    :param database:
+    :return:
+    """
+    conn = connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM states")
+    rows = cur.fetchall()
+    for id_, name in rows:
+        print(f"({id_}, '{name}')")
 
 
 if __name__ == "__main__":
-    conn_db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3])
-
-    cur = conn_db.cursor()
-
-    cur.execute("SELECT * FROM states")
-
-    query_rows = cur.fetchall()
-
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn_db.close()
+    argv = argv[1:]
+    if len(argv) != 3:
+        print(f"Usage: {argv[0]} username password database")
+        exit(1)
+    main(*argv)
