60e5e92e9fc6fc654f44e7c64a63dfafa377eae8
GoodnewsAkpan <goodnewsakpan123@gmail.com>
2024-04-29 19:42:12 +0100

    python network 0

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a DELETE request to the URL passed
-curl -s "$1" -X DELETE
+# a Bash script that takes in a URL and displays all HTTP methods the server will accept
+curl -Is "$1" | grep "Allow:" | cut -d ":" -f 2 | cut -c 2- | rev | cut -c 2- | rev
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that takes an URL and shows the Allowed OPTIONS
-curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
+# a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "${1}"
